90878d5d03cf9f5c66d6317e275248b5e88b41dc
Julien Enoch <946462+JEnoch@users.noreply.github.com>
2018-07-06 12:17:14 +0200

    Folders reorganization

--- src/yaks/yaks-daemon/yaksd.ml
+++ src/yaks/yaks-daemon/yaksd.ml
@@ -19,13 +19,22 @@ let setup_log =
   let env = Arg.env_var "YAKSD_VERBOSITY" in
   Term.(const setup_log $ Fmt_cli.style_renderer () $ Logs_cli.level ~env ())
 
+(*
 let yaksd () = 
   let ecfg = Engine.{channel_len = 32 } in
   let engine = Engine.create  ecfg in 
   let fecfg = Yaks_fe_sock.{ iface = "127.0.0.1"; port = 8448; backlog = 10; bufsize = 64000; stream_len = 32 } in   
   let fe = Yaks_fe_sock.create fecfg (Engine.event_sink engine) in
   Lwt.join [Engine.start engine;  Yaks_fe_sock.start fe]
-  
+*)
+let yaksd () = 
+  let ecfg = Engine.{channel_len = 32 } in
+  let engine = Engine.create  ecfg in 
+  let fecfg = Yaks_fe_rest.{ port = 8000; stream_len = 32 } in   
+  let fe = Yaks_fe_rest.create fecfg (Engine.event_sink engine) in
+  Lwt.join [Engine.start engine;  Yaks_fe_rest.start fe]
+
+
 let () =  
   let _ = Term.(eval (setup_log, Term.info "tool")) in  
   Lwt_main.run @@ yaksd ()
