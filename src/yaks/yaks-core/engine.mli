d9f7cdd4037987eeda9b6f96c1826d9608700fac
gabrik <gabriele.baldoni@gmail.com>
2018-07-26 13:09:10 +0200

    Implemented plugin loading/offloading in the engine through messages

--- src/yaks/yaks-core/engine.mli
+++ src/yaks/yaks-core/engine.mli
@@ -2,14 +2,22 @@ open Yaks_event
 open Actor
 
 module Engine : sig   
-  type t          
+  type t
+  type state
   type config = { channel_len : int }  
 
-  val create : config -> Yaks_event.event Actor.actor_mailbox -> t
+  (* val create : config -> Yaks_event.event Actor.actor_mailbox -> t *)
+  val create : config -> t
   (** [create] a Yaks engine and return the function to be called to push events *)
 
   val mailbox : t -> Yaks_event.event Actor.actor_mailbox
   (** More operations will be needed *)  
+
+
+
+  (* PRIVATE FUNCTIONS *)
+  val add_plugin : Yaks_event.event Actor.actor_mailbox -> Int64.t -> Yaks_event.plugin_kind -> state -> (bool * state)
+  val remove_plugin : Int64.t -> Yaks_event.plugin_kind -> state -> (bool * state)
 
   val start : t -> unit Lwt.t
 end
--- src/yaks/yaks-daemon/yaksd.ml
+++ src/yaks/yaks-daemon/yaksd.ml
@@ -29,7 +29,12 @@ let setup_log =
 let yaksd () = 
   let mm_actor, mm_loop = Yaks_be_mm.create "" in
   let ecfg = Engine.{channel_len = 32 } in
-  let engine = Engine.create ecfg mm_actor in 
+  (* let engine = Engine.create ecfg mm_actor in  *)
+  let engine = Engine.create ecfg in
+
+  let msg = Event (AddPlugin{entity=Plugin{mailbox=mm_actor; kind=Backend};entity_id=PluginId(0L)}) in
+  let _ = Actor.Actor.send (Engine.mailbox engine) None msg in
+  (* let _, e_state = Engine.add_febe mm_actor (FEBEId (1L)) Backend e_state in *)
   let sockfecfg = Yaks_fe_sock.{ iface = "127.0.0.1"; port = 8448; backlog = 10; bufsize = 64000; stream_len = 32 } in   
   let sockfe = Yaks_fe_sock.create sockfecfg (Engine.mailbox engine) in
   let restfecfg = Yaks_fe_rest.{ port = 8000 } in
