392a154faf82bdbe5a6ed3aa96621e3810c67ef0
Ndrianja Rakotomanga <ndrianja.rakotomanga@adlinktech.com>
2018-07-27 10:08:57 +0200

    updating test for java rest async api

--- src/api/java/yaks-driver-rest/src/test/java/is/yaks/rest/async/AccessTest.java
+++ src/api/java/yaks-driver-rest/src/test/java/is/yaks/rest/async/AccessTest.java
@@ -1,11 +1,9 @@
 package is.yaks.rest.async;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.function.Function;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -17,10 +15,10 @@ import org.slf4j.LoggerFactory;
 import is.yaks.Encoding;
 import is.yaks.Path;
 import is.yaks.Selector;
-import is.yaks.async.Storage;
 import is.yaks.async.Access;
+import is.yaks.async.Storage;
 import is.yaks.async.Yaks;
-import is.yaks.rest.async.YaksImpl;
+import is.yaks.rest.foo.Foo;
 
 public class AccessTest {
 
@@ -65,6 +63,12 @@ public class AccessTest {
 				.get().get();		
 		Assert.assertEquals(a,"ABC");
 		
+		//test put/get
+		Foo b = access1.put(Selector.ofString("//is.yaks.tests.async/b"), new Foo())
+				.thenApply((Access asyncAccess) ->{return asyncAccess.get(Path.ofString("//is.yaks.tests.async/b"), Foo.class);})
+				.get().get();
+		Assert.assertNotNull(b);
+
 		//get access with id
 		CompletableFuture<Access> futureAccess = yaks.getAccess("access-async-1");
 		Access assyncAccess = futureAccess.get();
@@ -94,7 +98,6 @@ public class AccessTest {
 		access.unsubscribe(subId);
 		access.dispose();
 	}
-
 
 	@After
 	public void stop() {
