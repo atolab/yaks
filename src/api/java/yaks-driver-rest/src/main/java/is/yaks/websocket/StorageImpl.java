940daac6e9f8bab3db2551b25de2b9316a727d9c
Ndrianja Rakotomanga <ndrianja.rakotomanga@adlinktech.com>
2018-07-30 15:52:28 +0200

    beginning on java api websocket

---
+++ src/api/java/yaks-driver-rest/src/main/java/is/yaks/websocket/StorageImpl.java
@@ -0,0 +1,13 @@
+package is.yaks.websocket;
+
+import is.yaks.Storage;
+
+public class StorageImpl implements Storage {
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
