ea5f046cc0081a67524d50525f18da32fe911188
gabrik <gabriele.baldoni@gmail.com>
2018-08-14 13:21:54 +0200

    updated sh file

--- .yaks-ci.sh
+++ .yaks-ci.sh
@@ -1,12 +1,4 @@
 #sh <(curl -sL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh)
 OPAM_DEPENDS="lwt dune logs fmt cmdliner cmdliner ppx_deriving uuidm yojson lwt_ppx lwt_log ppx_cstruct atdgen cohttp-lwt-unix"
 sh .travis-opam.sh
-WD=$PWD
-opam install ${OPAM_DEPENDS} -y
-cd ~
-git clone -b master https://github.com/atolab/apero
-make -C apero && make install -C apero 
-cd $WD
-make
-
 
