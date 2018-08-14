f0a029dc7c982c4a750db5bf8c35457aacf48146
gabrik <gabriele.baldoni@gmail.com>
2018-08-14 10:56:58 +0200

    updated travis.yml

--- .yaks-ci.sh
+++ .yaks-ci.sh
@@ -1,4 +1,4 @@
-sh <(curl -sL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh)
+#sh <(curl -sL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh)
 OPAM_DEPENDS="lwt dune logs fmt cmdliner cmdliner ppx_deriving uuidm yojson lwt_ppx lwt_log ppx_cstruct atdgen cohttp-lwt-unix"
 WD=$PWD
 eval `opam config env`
