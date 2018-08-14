885df21285f4433c3b00e1ea97b21c98db308890
gabrik <gabriele.baldoni@gmail.com>
2018-08-14 13:48:32 +0200

    clean up on sh file

--- .yaks-ci.sh
+++ .yaks-ci.sh
@@ -1,4 +1,2 @@
-#sh <(curl -sL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh)
-OPAM_DEPENDS="lwt dune logs fmt cmdliner cmdliner ppx_deriving uuidm yojson lwt_ppx lwt_log ppx_cstruct atdgen cohttp-lwt-unix"
 sh .travis-opam.sh
 
