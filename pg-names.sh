9ec8c73a232c24fcc8e161e686f9fbcd8c8ea34c
kydos <angelo@icorsaro.net>
2018-10-17 12:36:23 +0200

    improved pg-names stress test for REST FE

--- pg-names.sh
+++ pg-names.sh
@@ -1,15 +1,33 @@
-AID=$1
-NAMES=`cat /usr/share/dict/propernames`
+#!/bin/bash
 
+YAKS_URL="http://localhost:8000"
+if [ $# -lt 2 ]; then 
+  echo "Usage:"
+  echo "          pg-names.sh <access-id> <storage-path> [<yaks-url=$YAKS_URL>]"
+  echo ""
+  echo " To create an access use create-access.sh <access-paht>"
+  exit
+fi
+
+AID=$1
+STORAGE_PATH=$2
+
+GRAY='\033[0;37m'
+RED='\033[1;31m'
+BLUE='\033[1;34m'
+if [ $# -ge 4 ]; then 
+  YAKS_URL=$3
+fi
+
+NAMES=`cat /usr/share/dict/propernames`
+URL="http://localhost:8000//home/ac/$STORAGE_PATH"
 for name in ${NAMES}; do
-  echo "-"
-  echo "curl -X PUT --cookie is.yaks.access=${AID} -d {value:$name} http://localhost:8000/home/ac/${name}"
-  echo "-"
-  curl -X PUT --cookie is.yaks.access=${AID} -d {value:$name} http://localhost:8000/home/ac/${name}
+  echo -e "${RED}curl -X PUT --cookie is.yaks.access=${AID} -d {value:$name} $STORAGE_PATH/${name}"  
+  curl -X PUT --cookie is.yaks.access=${AID} -d {value:$name} ${YAKS_URL}${STORAGE_PATH}/${name}
 done
 
 for name in ${NAMES}; do
-  echo "curl -X GET --cookie is.yaks.access=${AID} http://localhost:8000/home/ac/${name}"
-  curl -X GET --cookie is.yaks.access=${AID} http://localhost:8000/home/ac/${name}
+  echo -e "${GRAY}curl -X GET --cookie is.yaks.access=${AID} ${YAKS_URL}${STORAGE_PATH}/${name} ${BLUE}"
+  curl -X GET --cookie is.yaks.access=${AID} ${YAKS_URL}${STORAGE_PATH}/${name}
   echo ""
 done
