4b20208fc89ce041b9a6a8317b8b46f937e9c5ed
kydos <angelo@icorsaro.net>
2018-10-24 16:00:21 +0200

    added usage

--- create_access.sh
+++ create_access.sh
@@ -1,7 +1,12 @@
+if [ $# -lt 1 ]; then
+    echo "USAGE:"
+    echo "         create-access.sh <access-path>"
+    exit
+fi 
 ACP=$1
 echo "Creating Access for ${ACP}"
 CSIZE=1024
 ALINE=`curl -v -d '' http://localhost:8000/yaks/access?path=${ACP}\&cacheSize=${CSIZE} 2>&1 | grep location`
 pfix="< location: "
 AID=${ALINE:${#pfix}}
-echo ${AID}
+echo -e "${BLUE}${AID}"
