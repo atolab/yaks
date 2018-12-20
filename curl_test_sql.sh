57f1f5a16057aaa2381748db293129d3ca128299
Julien Enoch <946462+JEnoch@users.noreply.github.com>
2018-12-20 20:43:14 +0100

    Add curl_test_sql.sh

---
+++ curl_test_sql.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+URL_BASE=http://localhost:8000
+URL_ADMIN=$URL_BASE/yaks/local
+
+
+function GET ()
+{
+    echo "GET $1"
+    curl $1
+    echo ""
+}
+
+function PUT ()
+{
+    echo "PUT $1"
+    curl -X PUT -d $2 $1
+}
+
+function DEL ()
+{
+    echo "DEL $1"
+    curl -X DELETE $1
+}
+
+# Get all from admin space
+GET $URL_ADMIN/**
+echo ">"; read
+
+# Create memory storage
+PUT $URL_ADMIN/backend/Memory/storage/s1 '{"path":"/is/test/mem"}' 
+
+# Create SQL storage on legacy table "test"
+PUT $URL_ADMIN/backend/SQL/storage/s2 '{"path":"/is/test/db/leg-table","is.yaks.backend.sql.table":"test"}' 
+
+# Create "auto" storage (using kind=dbms) on on a new key/value
+PUT $URL_ADMIN/backend/auto/storage/s3 '{"path":"/is/test/db/new-table","is.yaks.backend.kind":"dbms","is.yaks.backend.sql.on_dispose":"drop"}'
+
+# Get all from admin space
+GET $URL_ADMIN/**
+echo ">"; read
+
+####### SQL storage - key/value table #######
+echo "==== Tests on key/value table (/is/test/db/new-table)"
+PUT $URL_BASE/is/test/db/new-table/A/B 'BCD' 
+PUT $URL_BASE/is/test/db/new-table/A/D 'DEF' 
+PUT $URL_BASE/is/test/db/new-table/A/B/G 'GHI' 
+PUT $URL_BASE/is/test/db/new-table/A/B/H/I/JKL 'JKL' 
+
+GET $URL_BASE/is/test/db/new-table/A/B
+GET $URL_BASE/is/test/db/new-table/A/*
+GET $URL_BASE/is/test/db/new-table/A/**
+GET $URL_BASE/is/test/db/new-table/A/**/J*
+echo ">"; read
+
+# replacement
+GET $URL_BASE/is/test/db/new-table/A/B/G
+PUT $URL_BASE/is/test/db/new-table/A/B/G 'XXXX' 
+GET $URL_BASE/is/test/db/new-table/A/B/G
+
+# deletion
+GET $URL_BASE/is/test/db/new-table/A/*
+DEL $URL_BASE/is/test/db/new-table/A/D
+GET $URL_BASE/is/test/db/new-table/A/*
+
+####### SQL storage - legacy table #######
+echo "==== Tests on legacy table (/is/test/db/leg-table)"
+GET $URL_BASE/is/test/db/leg-table
+echo ">"; read
+
